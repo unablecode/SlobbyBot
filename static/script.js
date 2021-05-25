4fde410c86196204389e3671d9a246ec1ad0fc53
Flurry58 <51930471+Flurry58@users.noreply.github.com>
2021-05-25 08:25:52 -0400

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,16 +1,1 @@
-$(document).ready(function() {
-  $('.toggle').click(function() {
-   var current_status = $('.status').text();
-   $.ajax({
-    url: "/get_toggled_status",
-    type: "get",
-     data: {status: current_status},
-     success: function(response) {
-      $(".status").html(response);
-     },
-     error: function(xhr) {
-      //Do Something to handle error
-     }
-   });
-  });
-});
+//stuff here

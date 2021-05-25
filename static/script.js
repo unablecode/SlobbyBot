a26da0a69fcc10e59a29bbf40cb1674ef044e664
Flurry58 <51930471+Flurry58@users.noreply.github.com>
2021-05-25 08:16:39 -0400

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
   $('.toggle').click(function() {
-    var current_status = $('.status').text();
-    if (current_status === 'Untoggled'){
-      $('.status').html('Toggled');
-    } else {
-      $('.status').html('Untoggled');
-    }
+   var current_status = $('.status').text();
+   $.ajax({
+    url: "/get_toggled_status",
+    type: "get",
+     data: {status: current_status},
+     success: function(response) {
+      $(".status").html(response);
+     },
+     error: function(xhr) {
+      //Do Something to handle error
+     }
+   });
   });
 });

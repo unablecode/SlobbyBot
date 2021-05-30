28dade52311b5513247b714c1570c007068e76cd
Flurry58 <51930471+Flurry58@users.noreply.github.com>
2021-05-30 16:11:35 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -328,6 +328,7 @@ async def add_experience(user):
 
 async def level_up(user, username):
 	response = requests.get('https://Test-1.loganpollack.repl.co', params={'file': 'users','function': 'level_up', 'author': username})
+	output = response.json()
 	lvl_end = int(output['end'])
 	lvl_start = int(output['start'])
 	if lvl_start < lvl_end:
